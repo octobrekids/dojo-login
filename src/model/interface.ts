b07ce6e3c9d8ab3abc4835903b5c0d12f9602967
octobrekids <chnrd.k@gmail.com>
2020-09-25 02:04:45 +0700

    Add FormState Interface

--- src/model/interface.ts
+++ src/model/interface.ts
@@ -1,4 +1,4 @@
-export default interface FormValue {
+export  interface FormValue {
   value: string;
   name: string;
 }
@@ -11,6 +11,15 @@ export type UseLogin = [
   (e: React.ChangeEvent<HTMLInputElement>) => void,
   () => void
 ];
+
+export interface FormState {
+        values: FormValue[],
+        errors: boolean,
+        loading: boolean,
+        loggedIn: boolean,
+        onChange:(e: React.ChangeEvent<HTMLInputElement>) => void,
+        onSubmit:() => void, 
+}
 
 export default interface LoginState {
   type: "login" | "loading" | "success" | "error";
